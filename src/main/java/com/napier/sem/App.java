5e8db9aeb53efc74a6fc0acfee3527f5bb602954
Julia Swietochowska <julkaswietochowska@gmail.com>
2022-02-18 00:12:07 +0000

    Complete getSalaries task, prepare for release 4

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -19,6 +19,9 @@ public class App
         // Test the size of the returned data - should be 240124
         System.out.println(employees.size());
 
+        // Display all the employees and their salaries
+        a.printSalaries(employees);
+
         // Disconnect from database
         a.disconnect();
     }
@@ -59,7 +62,7 @@ public class App
             }
             catch (SQLException sqle)
             {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println("Failed to connect to database attempt " + i);
                 System.out.println(sqle.getMessage());
             }
             catch (InterruptedException ie)
@@ -211,6 +214,25 @@ public class App
             System.out.println(e.getMessage());
             System.out.println("Failed to get salary details");
             return null;
+        }
+    }
+
+    /**
+     * Prints a list of employees and their salaries
+     * @param employees The list of employees to print
+     */
+    public void printSalaries(ArrayList<Employee> employees)
+    {
+        // Print header
+        // String.format() puts the data into columns of specified sizes
+        System.out.println(String.format("%-10s %-15s %-20s %-8s", "Emp No", "First Name", "Last Name", "Salary"));
+        // Loop over all employees in the list
+        for(Employee emp : employees)
+        {
+            String emp_string = String.format("%-10s %-15s %-20s %-8s", emp.emp_no, emp.first_name, emp.last_name,
+                    emp.salary);
+            System.out.println(emp_string);
+
         }
     }
 }
